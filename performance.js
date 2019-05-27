862d5256c8e27bfa8c825b08d694cee167f5d081
stampyzfanz <joshua.hanbury@education.nsw.gov.au>
2019-05-27 16:50:02 +1000

    Check performance of drawImg. (~14000ms for normal image)

---
+++ performance.js
@@ -0,0 +1,20 @@
+function testDrawImgSpeed() {
+	const iterations = 3
+	let avg = [];
+
+	for (let i = 0; i < iterations; i++) {
+		let start = performance.now();
+		drawImg('normalImg');
+		let end = performance.now();
+		let elapsed = end - start;
+		console.log(`drawImg with normalImg number ${i}: ${elapsed}`);
+		avg.push(elapsed);
+	}
+
+	let sum = 0;
+	for (let n of avg) {
+		sum += n;
+	}
+	avg = sum / iterations; // now an int
+	console.log('drawImg with normalImg avg: ' + avg);
+}
